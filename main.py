7bf25d0531b30047050722db4cf15c4e23e98882
Prakher Pratyush <prakher278@gmail.com>
2021-02-10 00:49:43 +0530

    Add try and except blocks
    Adding try and except blocks, while fetching data from Jira. We are fetching data from multiple projects and these projects may have different field configuration screens. This will give errors while fetching data for a field not added in the field configuration screen of a particular project.
    Also, this prevents the error we get if we are getting no value for multiple select fields.

--- main.py
+++ main.py
@@ -11,9 +11,24 @@ with open('jiradump.csv', 'wb') as f:
     thewriter.writerow(
         ['id', 'issuenum', 'issue_type', 'key', 'summary', 'assignee', 'priority', 'status', 'created', 'updated',
          'resolution', 'linkedissue', 'countries', 'domains', 'clarity_ID', 'vendor_management'])
+    
+    issue_type = ""                                                                                       
+    summary = ""
+    assignee = ""
+    priority = ""
+    status = ""
+    resolution = ""
+    countries = []
+    domains = []
+    clarity_ID = ""
+    vendor_management = ""
+    created = ""
+    updated = ""
+    golivedate = ""
+    linkedissue = []
 
     # JQL to fetch issues
-    for issue in jira.search_issues('project=TEST order by Created ASC'):
+    for issue in jira.search_issues('project in (TEST,DEV) order by Created ASC'):
 
         # Issue Id and Key
         id = issue.id
@@ -23,41 +38,92 @@ with open('jiradump.csv', 'wb') as f:
         issuenum = issue.key.split("-")[1]
 
         # Normal Fields
-        issue_type = issue.fields.issuetype
-        summary = issue.fields.summary
-        assignee = issue.fields.assignee
-        priority = issue.fields.priority
-        status = issue.fields.status
-        resolution = issue.fields.resolution
+        try:                                                                                               
+            issue_type = issue.fields.issuetype
+        except:                                                                                            
+            print("issue_type : " + id)
+
+        try:
+            summary = issue.fields.summary
+        except:
+            print("summary : " + id)
+
+        try:
+            assignee = issue.fields.assignee
+        except:
+            print("assignee : " + id)
+
+        try:
+            priority = issue.fields.priority
+        except:
+            print("priority : " + id)
+
+        try:
+            status = issue.fields.status
+        except:
+            print("status : " + id)
+
+        try:
+            resolution = issue.fields.resolution
+        except:
+            print("resolution : " + id)
 
         # Multiselect Custom Fields
-        countries_array = issue.fields.customfield_10200
-        countries = []
-        for country in countries_array:
-            countries.append((country.value).encode('ascii'))
+        try:
+            countries_array = issue.fields.customfield_10200
+            countries = []                                                                                 
+            for country in countries_array:
+                countries.append(country.value.encode('ascii'))
+        except:
+            print("countries : " + id)
 
-        domains_array = issue.fields.customfield_10201
-        domains = []
-        for domain in domains_array:
-            domains.append((domain.value).encode('ascii'))
+        try:
+            domains_array = issue.fields.customfield_10201
+            domains = []                                                                                   
+            for domain in domains_array:
+                domains.append(domain.value.encode('ascii'))
+        except:
+            print("domains : " + id)
 
         # Normal Custom Fields
-        clarity_ID = issue.fields.customfield_10202
-        vendor_management = issue.fields.customfield_10203
+        try:
+            clarity_ID = issue.fields.customfield_10202
+        except:
+            print("clarity_ID : " + id)
+
+        try:
+            vendor_management = issue.fields.customfield_10203
+        except:
+            print("vendor_management : " + id)
 
         # Date Field
-        created = dateutil.parser.parse(issue.fields.created)
-        updated = dateutil.parser.parse(issue.fields.updated)
+        try:
+            created = dateutil.parser.parse(issue.fields.created)
+        except:
+            print("created : " + id)
+
+        try:
+            updated = dateutil.parser.parse(issue.fields.updated)
+        except:
+            print("updated : " + id)
+
+        try:
+            golivedate = dateutil.parser.parse(issue.fields.customfield_10300)
+        except:
+            print("golivedate : " + id)
 
         # Linked Issues
-        linkedissue = []
-        for link in issue.fields.issuelinks:
-            if hasattr(link, "outwardIssue"):
-                linkedissue.append((link.outwardIssue.key).encode('ascii'))
-            if hasattr(link, "inwardIssue"):
-                linkedissue.append((link.inwardIssue.key).encode('ascii'))
+        try:
+            linkedissue = []                                                                               
+            for link in issue.fields.issuelinks:
+                if hasattr(link, "outwardIssue"):
+                    linkedissue.append(link.outwardIssue.key.encode('ascii'))
+                if hasattr(link, "inwardIssue"):
+                    linkedissue.append(link.inwardIssue.key.encode('ascii'))
+        except:
+            print("linkedissue : " + id)
 
         # Add data in CSV
         thewriter.writerow(
             [id, issuenum, issue_type, key, summary, assignee, priority, status, created, updated, resolution,
-             linkedissue, countries, domains, clarity_ID, vendor_management])
+             linkedissue, countries, domains, clarity_ID, vendor_management, golivedate])
