d13f86a38b272837ae943f5c839bbdb5b4dd15d7
Prakher Pratyush <prakher278@gmail.com>
2021-02-11 23:24:22 +0530

    Adding quoting in created csv
    Added quoting=csv.QUOTE_ALL in thewriter = csv.writer(f, quoting=csv.QUOTE_ALL) because I want to add quotes for all fields in created csv file.

--- main.py
+++ main.py
@@ -3,29 +3,29 @@ import dateutil.parser
 import csv
 
 # Initialization
-jira = JIRA(basic_auth=('praprakher', 'XXXXXXX'), options={"server": "http://localhost:8080/"})
+jira = JIRA(basic_auth=('praprakher', 'Test@123'), options={'server': 'http://localhost:8080/'})
 
 # Create CSV
 with open('jiradump.csv', 'wb') as f:
-    thewriter = csv.writer(f)
+    thewriter = csv.writer(f, quoting=csv.QUOTE_ALL)                                                       # important
     thewriter.writerow(
         ['id', 'issuenum', 'issue_type', 'key', 'summary', 'assignee', 'priority', 'status', 'created', 'updated',
-         'resolution', 'linkedissue', 'countries', 'domains', 'clarity_ID', 'vendor_management'])
-    
-    issue_type = ""                                                                                       
-    summary = ""
-    assignee = ""
-    priority = ""
-    status = ""
-    resolution = ""
-    countries = []
-    domains = []
-    clarity_ID = ""
-    vendor_management = ""
-    created = ""
-    updated = ""
-    golivedate = ""
-    linkedissue = []
+         'resolution', 'linkedissue', 'countries', 'domains', 'clarity_ID', 'vendor_management', 'Go-live Date'])
+
+    issue_type = ''                                                                                         # important
+    summary = ''
+    assignee = ''
+    priority = ''
+    status = ''
+    resolution = ''
+    countries = ''
+    domains = ''
+    clarity_ID = ''
+    vendor_management = ''
+    created = ''
+    updated = ''
+    golivedate = ''
+    linkedissue = ''
 
     # JQL to fetch issues
     for issue in jira.search_issues('project in (TEST,DEV) order by Created ASC'):
@@ -35,93 +35,105 @@ with open('jiradump.csv', 'wb') as f:
         key = issue.key
 
         # Issue Number
-        issuenum = issue.key.split("-")[1]
+        issuenum = issue.key.split('-')[1]
 
         # Normal Fields
-        try:                                                                                               
+        try:                                                                                                # important
             issue_type = issue.fields.issuetype
-        except:                                                                                            
-            print("issue_type : " + id)
+        except:                                                                                             # important
+            print('issue_type : ' + id)
 
         try:
             summary = issue.fields.summary
         except:
-            print("summary : " + id)
+            print('summary : ' + id)
 
         try:
             assignee = issue.fields.assignee
         except:
-            print("assignee : " + id)
+            print('assignee : ' + id)
 
         try:
             priority = issue.fields.priority
         except:
-            print("priority : " + id)
+            print('priority : ' + id)
 
         try:
             status = issue.fields.status
         except:
-            print("status : " + id)
+            print('status : ' + id)
 
         try:
             resolution = issue.fields.resolution
         except:
-            print("resolution : " + id)
+            print('resolution : ' + id)
 
         # Multiselect Custom Fields
         try:
             countries_array = issue.fields.customfield_10200
-            countries = []                                                                                 
+            countries = ''                                                                                  # important
             for country in countries_array:
-                countries.append(country.value.encode('ascii'))
+                if countries == '':
+                    countries = country.value
+                else:
+                    countries = countries + ',' + country.value
         except:
-            print("countries : " + id)
+            print('countries : ' + id)
 
         try:
             domains_array = issue.fields.customfield_10201
-            domains = []                                                                                   
+            domains = ''                                                                                    # important
             for domain in domains_array:
-                domains.append(domain.value.encode('ascii'))
+                if domains == '':
+                    domains = domain.value
+                else:
+                    domains = domains + ',' + domain.value
         except:
-            print("domains : " + id)
+            print('domains : ' + id)
 
         # Normal Custom Fields
         try:
             clarity_ID = issue.fields.customfield_10202
         except:
-            print("clarity_ID : " + id)
+            print('clarity_ID : ' + id)
 
         try:
             vendor_management = issue.fields.customfield_10203
         except:
-            print("vendor_management : " + id)
+            print('vendor_management : ' + id)
 
         # Date Field
         try:
             created = dateutil.parser.parse(issue.fields.created)
         except:
-            print("created : " + id)
+            print('created : ' + id)
 
         try:
             updated = dateutil.parser.parse(issue.fields.updated)
         except:
-            print("updated : " + id)
+            print('updated : ' + id)
 
         try:
             golivedate = dateutil.parser.parse(issue.fields.customfield_10300)
         except:
-            print("golivedate : " + id)
+            print('golivedate : ' + id)
 
         # Linked Issues
         try:
-            linkedissue = []                                                                               
+            linkedissue = ''                                                                                # important
             for link in issue.fields.issuelinks:
-                if hasattr(link, "outwardIssue"):
-                    linkedissue.append(link.outwardIssue.key.encode('ascii'))
-                if hasattr(link, "inwardIssue"):
-                    linkedissue.append(link.inwardIssue.key.encode('ascii'))
+                if linkedissue == '':
+                    if hasattr(link, 'outwardIssue'):
+                        linkedissue = link.outwardIssue.key
+                    if hasattr(link, 'inwardIssue'):
+                        linkedissue = link.inwardIssue.key
+                else:
+                    if hasattr(link, 'outwardIssue'):
+                        linkedissue = linkedissue + ',' + link.outwardIssue.key
+                    if hasattr(link, 'inwardIssue'):
+                        linkedissue = linkedissue + ',' + link.inwardIssue.key
         except:
-            print("linkedissue : " + id)
+            print('linkedissue : ' + id)
 
         # Add data in CSV
         thewriter.writerow(
